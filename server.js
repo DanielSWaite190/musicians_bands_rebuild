3a05dae42c36429021b42f7583720c7b6dc72bd1
DanielSWaite190 <danielwaite190@gmail.com>
2022-07-04 11:01:46 -0700

    initial set up

---
+++ server.js
@@ -0,0 +1,20 @@
+//Set the curent file name
+const path = require('path');
+const curentFile = path.basename(__filename);
+
+//Express configuration
+const express = require('express');
+const app = express()
+const port = 3000;
+
+//Inishiate server
+app.listen(port, () => {console.log(`musicians-bands(rebuilt) app, ${curentFile}`)})
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+//Routes
+app.get('/', (request, response) => {
+    response.json("Hi Mom")
+})
